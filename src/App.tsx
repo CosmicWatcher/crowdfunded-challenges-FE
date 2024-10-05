6f9b9c337401d72fa2e040ba8692c1558f4c5bba
CosmicWatcher-Mac <>
2024-10-05 19:05:57 -0400

    added error boundary for the whole app

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,43 @@
 import { Route, Switch } from "wouter";
 import { DollarSign } from "lucide-react";
-
+import { ErrorBoundary } from "react-error-boundary";
 import { TaskPage } from "@/pages/TaskPage";
 import { SignupPage } from "@/pages/SignupPage";
 import { LoginPage } from "@/pages/LoginPage";
 import { AppLayout } from "@/components/layout/AppLayout";
 import { ThemeProvider } from "@/components/theme/provider";
 import { SitePages } from "@/configs/routes";
+import { MainErrorFallback } from "@/components/error/main";
 
 export default function App() {
   return (
-    <ThemeProvider storageKey="vite-ui-theme">
-      <AppLayout>
-        <Switch>
-          <Route path={SitePages.HOME}>
-            <DollarSign className="animate-ping fixed left-1/3 top-1/3 size-32" />
-            <DollarSign className="animate-ping fixed left-1/4 top-1/4 size-16" />
-            <DollarSign className="animate-ping fixed right-1/4 top-2/3 size-20" />
-            <DollarSign className="animate-ping fixed left-1/4 top-3/4 size-12" />
-            <DollarSign className="animate-ping fixed left-1/2 top-2/3 size-4" />
-            <DollarSign className="animate-ping fixed left-2/3 top-1/3 size-8" />
-          </Route>
-          <Route path={SitePages.SIGNUP}>
-            <SignupPage />
-          </Route>
-          <Route path={SitePages.LOGIN}>
-            <LoginPage />
-          </Route>
-          <Route path={SitePages.TASK}>
-            <TaskPage />
-          </Route>
-          <Route>
-            <h1 className="fixed left-1/2 top-1/4">404, Not Found!</h1>
-          </Route>
-        </Switch>
-      </AppLayout>
-    </ThemeProvider>
+    <ErrorBoundary fallback={<MainErrorFallback />}>
+      <ThemeProvider storageKey="vite-ui-theme">
+        <AppLayout>
+          <Switch>
+            <Route path={SitePages.HOME}>
+              <DollarSign className="animate-ping fixed left-1/3 top-1/3 size-32" />
+              <DollarSign className="animate-ping fixed left-1/4 top-1/4 size-16" />
+              <DollarSign className="animate-ping fixed right-1/4 top-2/3 size-20" />
+              <DollarSign className="animate-ping fixed left-1/4 top-3/4 size-12" />
+              <DollarSign className="animate-ping fixed left-1/2 top-2/3 size-4" />
+              <DollarSign className="animate-ping fixed left-2/3 top-1/3 size-8" />
+            </Route>
+            <Route path={SitePages.SIGNUP}>
+              <SignupPage />
+            </Route>
+            <Route path={SitePages.LOGIN}>
+              <LoginPage />
+            </Route>
+            <Route path={SitePages.TASK}>
+              <TaskPage />
+            </Route>
+            <Route>
+              <h1 className="fixed left-1/2 top-1/4">404, Not Found!</h1>
+            </Route>
+          </Switch>
+        </AppLayout>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
---
+++ src/components/error/main.tsx
@@ -0,0 +1,23 @@
+import { AlertCircle, Frown } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { SitePages } from "@/configs/routes";
+import { Button } from "@/components/ui/button";
+
+export function MainErrorFallback() {
+  return (
+    <Alert variant="destructive">
+      <AlertCircle className="size-5" />
+      <AlertTitle>Error</AlertTitle>
+      <AlertDescription>
+        Ooooops, something went wrong <Frown className="inline size-5" />
+      </AlertDescription>
+      <br></br>
+      <AlertDescription>
+        <Button onClick={() => window.location.assign(SitePages.HOME)}>
+          Go Back Home!
+        </Button>
+      </AlertDescription>
+    </Alert>
+  );
+}
