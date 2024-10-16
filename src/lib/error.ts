20fccee382023454cb8285d0194ffadd05b009f0
CosmicWatcher-Mac <>
2024-10-16 19:56:16 -0400

    prevent duplicate errors

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,11 +1,17 @@
 import { toast } from "react-toastify";
 
-export function handleError(error: unknown, prefix = "Error") {
+export function handleError(
+  error: unknown,
+  prefix = "Error",
+  toastId:
+    | string
+    | undefined = `${String(error)}-${Math.ceil(Date.now() / 10000)}`, // prevent duplicates within a 10 sec window
+) {
   console.error(prefix, error);
 
   let errorContent = "";
   if (error instanceof Error) errorContent = error.message;
   else errorContent = String(error);
 
-  toast.error(`${prefix}: ${errorContent}`, { autoClose: 20000 });
+  toast.error(`${prefix}: ${errorContent}`, { autoClose: 20000, toastId });
 }
