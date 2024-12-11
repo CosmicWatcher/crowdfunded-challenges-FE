4a6277d21196f59d14f2918a5abc079c8f254fa0
CosmicWatcher <aryan.kh88@gmail.com>
2024-12-11 14:09:58 -0500

    email signup redirect

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { Session, createClient } from "@supabase/supabase-js";
 
-import { SUPABASE_ANON_KEY, SUPABASE_URL } from "@/configs/env";
+import { APP_URL, SUPABASE_ANON_KEY, SUPABASE_URL } from "@/configs/env";
+import { SITE_PAGES } from "@/configs/routes";
 
 // Create a single supabase client for interacting with the database
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
@@ -29,6 +30,9 @@ export async function signup(email: string, password: string) {
   const { error } = await supabase.auth.signUp({
     email,
     password,
+    options: {
+      emailRedirectTo: `${APP_URL}/${SITE_PAGES.ACCOUNT}`,
+    },
   });
   if (error) {
     throw error;
