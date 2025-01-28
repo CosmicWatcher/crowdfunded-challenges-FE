093a7d20106e86568caabffde9f3c2039f253e3e
CosmicWatcher <aryan.kh88@gmail.com>
2025-01-28 15:08:20 -0500

    Env config: Remove trailing slashes from URLs

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,5 +1,18 @@
+function removeTrailingSlash(url: string) {
+  return url.replace(/\/$/, "");
+}
+
 export const SUPABASE_ANON_KEY = import.meta.env
   .VITE_SUPABASE_ANON_KEY as string;
-export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL as string;
-export const APP_URL = import.meta.env.VITE_APP_URL as string;
+
+export const SUPABASE_URL = removeTrailingSlash(
+  import.meta.env.VITE_SUPABASE_URL as string,
+);
+
+export const SERVER_URL = removeTrailingSlash(
+  import.meta.env.VITE_SERVER_URL as string,
+);
+
+export const APP_URL = removeTrailingSlash(
+  import.meta.env.VITE_APP_URL as string,
+);
